1b37f591ddf8be82e9217de52cfde160fdcb73e8
Ben <benniarnold@web.de>
2025-07-28 16:23:19 +0200

    add console.log

--- hooks/android/after_prepare.js
+++ hooks/android/after_prepare.js
@@ -3,11 +3,14 @@ const path = require('path');
 
 // Enable support for AndroidX & Jetifier
 function androidXUpgrade (context) {
+    console.log("Start after prepare hook!);
     const androidPlatformRoot = path.join(context.opts.projectRoot, 'platforms/android');
     const gradlePropertiesPath = path.join(androidPlatformRoot, 'gradle.properties');
-    
+
+    console.log("Read gradle properties");
     let gradlePropertiesStr = fs.readFileSync(gradlePropertiesPath, 'utf-8');
     if (gradlePropertiesStr) {
+        console.log("Found gradle log!");
         const enableAndroidX = "android.useAndroidX=true";
         const enableJetifier = "android.enableJetifier=true";
         const isAndroidXEnabled = gradlePropertiesStr.includes(enableAndroidX);
@@ -21,8 +24,9 @@ function androidXUpgrade (context) {
 
         if (isJetifierEnabled === false)
             gradlePropertiesStr += "\n" + enableJetifier;
-
+        console.log("Write gradle log");
         fs.writeFileSync(gradlePropertiesPath, gradlePropertiesStr, 'utf-8');
+        console.log("End after prepare hook!");
     }
 }
 
